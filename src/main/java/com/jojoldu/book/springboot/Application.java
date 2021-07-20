c0775e7c7474d8b27ebe2b61bfa161d15950576a
Hanseunghoon <3246902@naver.com>
2021-07-20 21:51:19 +0900

    Test HelloController

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,18 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    /*
+        Application : 프로젝트의 메인 클래스
+            @SpringBootApplication : 스프링 부트의 자동 설정, 스프링 Bean 읽기/생성 모두 자동 설정
+                                     해당 어노테이션이 있는 위치부터 설정을 읽음 > 항상 프로젝트 최상단에 존재!
+     */
+    public static void main(String[] args) {
+        // 내장 WAS를 실행 (별도로 외부에 WAS를 두지 않고 애플리케이션 실행 시 내부 WAS를 실행
+        // 항상 서버에 톰캣을 설치할 필요 X
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,62 @@
+package com.jojoldu.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/*  1. @RunWith(SpringRunner.class)
+    테스트 진행 시 JUnit에 내장된 실행자 외에 다른 실행자를 실행
+    SpringRunner라는 스프링 실행자 사용
+        즉, 스프링 부트 테스트와 JUnit 사이 연결자 역할
+
+    2. @WebMvcTest
+    여러 스프링 테스트 어노테이션 중 Web에 집중할 수 있는 어노테이션
+    선언 시 @Controller, @ControllerAdvice 등 사용 o
+           @Service, @Component, @Repository 등 사용 x
+    여기서는 컨트롤러만 사용하기 때문에 선언
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+    /* 
+        3. @Autowired
+        스프링이 관리하는 빈(Bean)을 자동으로 주입 받음
+        
+        4. private MockMvc mvc
+        웹 API를 테스트할 때 사용
+        스프링 MVC 테스트 시작점
+        이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트 수행 가능
+    */
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+        /*
+            5. mvc.perform(get("/hello"))
+            MockMvc를 통해 /hello 주소로 HTTP GET 요청
+            체이닝이 지원되어 아래와 같이 여러 검증 기능을 이어서 선언 가능 (.andExpect)
+
+            6. .andExpect(status().isOk())
+            mvc.perform의 결과를 검증
+            HTTP Header의 Status를 검증
+            우리가 흔히 알고 있는 200, 404, 500 등의 상태를 검증
+            OK (200) 인지 아닌지를 검증
+
+            7. .andExpect(content().string(hello))
+            mvc.perform의 결과를 검증
+            응답 본문의 내용 검증
+            Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증
+        */
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
